1c9c8c870ecf077f2ad6bd57127a301d1f1ccfc9
Dapabot <101037424+Dapabot@users.noreply.github.com>
2022-03-17 19:43:33 +0700

    Update and rename plugins/topuppubg.js to plugin/topuppubg.js

--- plugin/topuppubg.js
+++ plugin/topuppubg.js
@@ -4,8 +4,8 @@ const topuppubg = (prefix) => {
 *LIST UC PUBG*
 *NO ILEGAL*
 - 🎟️60 = Rp14.000
-- 🎟️120 = Rp28.000
-- 🎟️240 = Rp56.000
+- 🎟️120 = Rp27.000
+- 🎟️240 = Rp57.500
 *minat? hub :* http://wa.me/${nomorowner}
 `
 }
