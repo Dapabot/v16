0e027273d3b829e6ac5ef6f79cabdf242e78708d
Dapabot <101037424+Dapabot@users.noreply.github.com>
2022-03-17 19:36:23 +0700

    Update and rename plugins/topupdana.js to plugin/topupdana.js

---
+++ plugin/topupdana.js
@@ -0,0 +1,19 @@
+const topupdana = (prefix) => { 
+	return `*HALLO KAKðŸ‘‹*
+*PILIH DIBAWAH YA!!!*
+• 5K
+• 10K
+• 20K
+• 30K
+• 40K
+• 50K
+• 60K
+• 70K
+• 80K
+• 90K
+• 100K
+*minat? hub :* http://wa.me/${nomorowner}
+`
+}
+
+exports.topupdana = topupdana
